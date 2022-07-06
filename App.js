539dab6c6e41abbe86cc1d3601c06877798047db
Doyun <doyun@talktomeinkorean.com>
2022-07-06 23:14:12 +0900

    setting : style practice start

--- App.js
+++ App.js
@@ -5,6 +5,8 @@ import {
   StatusBar,
   StyleSheet,
   Text,
+  TextInput,
+  Button,
   useColorScheme,
   View,
 } from 'react-native';
@@ -13,8 +15,12 @@ const App = () => {
   return (
     <SafeAreaView>
       <StatusBar />
-      <View>
-        <Text>Hello World</Text>
+      <View style={styles.sectionContainer}>
+        <Text style={styles.highlight}>Code for Beginners</Text>
+        <View style={styles.flexRowContainer}>
+          <TextInput style={styles.input} value="its a text input" />
+          <Button title="Post" style={styles.postButton} />
+        </View>
       </View>
     </SafeAreaView>
   );
@@ -23,7 +29,14 @@ const App = () => {
 const styles = StyleSheet.create({
   sectionContainer: {
     marginTop: 32,
-    paddingHorizontal: 24,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  flexRowContainer: {
+    flexDirection: 'row',
+    alignItems: 'stretch',
+    justifyContent: 'center',
+    marginTop: 10,
   },
   sectionTitle: {
     fontSize: 24,
@@ -33,6 +46,20 @@ const styles = StyleSheet.create({
     marginTop: 8,
     fontSize: 18,
     fontWeight: '400',
+  },
+  input: {
+    borderColor: 'gray',
+    borderWidth: 1,
+    flex: 1,
+    paddingVertical: 10,
+    paddingHorizontal: 50,
+    textAlign: 'center',
+  },
+  postButton: {
+    flex: 1,
+    marginLeft: 10,
+    borderColor: 'gray',
+    borderWidth: 1,
   },
   highlight: {
     fontWeight: '700',

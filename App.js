0d8732b1ddeced8231f9e1be7cde0c2af83df585
Doyun <doyun@talktomeinkorean.com>
2022-07-13 14:14:11 +0900

    fix: Component Separation Finish

--- App.js
+++ App.js
@@ -1,16 +1,10 @@
 import React, {useState} from 'react';
 import {
   SafeAreaView,
-  FlatList,
   StatusBar,
-  StyleSheet,
-  Text,
-  TextInput,
-  Button,
-  View,
-  Pressable,
 } from 'react-native';
 import PostInput from './components/PostInput';
+import PostList from './components/PostList';
 import Title from './components/Title';
 
 const App = () => {
@@ -34,62 +28,9 @@ const App = () => {
       <StatusBar />
       <Title/>      
       <PostInput inputText={inputText} setInputText={setInputText} addPost={addPost} />
-      <FlatList
-        data={postList}
-        renderItem={post => {
-          return (
-            <Pressable style={styles.postListContainer} onPress={() => deletePost(post.item.id)}>
-              <Text style={styles.postElement}>{post.item.title}</Text>
-            </Pressable>
-          );
-        }}
-        keyExtractor={item => item.id}
-      />
+      <PostList postList={postList} deletePost={deletePost}/>
     </SafeAreaView>
   );
 };
-
-const styles = StyleSheet.create({
-  containerCentered: {
-    marginTop: 32,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  flexRowContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginTop: 10,
-    marginHorizontal: 10,
-    borderBottomColor: '#ccc',
-    borderBottomWidth: 1,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  postListContainer: {
-    alignItems: 'center',
-    marginHorizontal: 10,
-  },
-  postElement: {
-    marginTop: 10,
-    marginHorizontal: 10,
-    backgroundColor: '#90caf9',
-    color: 'white',
-    paddingVertical: 10,
-    width: '100%',
-    textAlign: 'center',
-    borderRadius: 10,
-  },
-});
 
 export default App;
--- components/PostList.js
+++ components/PostList.js
@@ -1,12 +1,37 @@
-import { View, Text } from 'react-native'
+import { Text, FlatList, Pressable, StyleSheet } from 'react-native'
 import React from 'react'
 
-const PostList = () => {
+const PostList = (props) => {
   return (
-    <View>
-      <Text>PostList</Text>
-    </View>
+    <FlatList
+        data={props.postList}
+        renderItem={post => {
+          return (
+            <Pressable style={styles.postListContainer} onPress={() => props.deletePost(post.item.id)}>
+              <Text style={styles.postElement}>{post.item.title}</Text>
+            </Pressable>
+          );
+        }}
+        keyExtractor={item => item.id}
+      />
   )
 }
+
+const styles = StyleSheet.create({
+  postListContainer: {
+    alignItems: 'center',
+    marginHorizontal: 10,
+  },
+  postElement: {
+    marginTop: 10,
+    marginHorizontal: 10,
+    backgroundColor: '#90caf9',
+    color: 'white',
+    paddingVertical: 10,
+    width: '100%',
+    textAlign: 'center',
+    borderRadius: 10,
+  },
+});
 
 export default PostList
